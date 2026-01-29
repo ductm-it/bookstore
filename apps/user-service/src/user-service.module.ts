18757b9776c3fcc4e66b56dd65f7137c71cfdcbe
ductm-it <duc.tran@southtelecom.vn>
2026-01-29 16:54:26 +0700

    add postgresql

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserServiceController } from './user-service.controller';
 import { UserServiceService } from './user-service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserServiceController],
   providers: [UserServiceService],
 })
