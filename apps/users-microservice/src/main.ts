47674552752183cc190e44ea1ad93193bbd85ae5
ductm-it <tranminhduc1299@gmail.com>
2026-01-30 11:14:36 +0700

    add users service

--- apps/users-microservice/src/main.ts
+++ apps/users-microservice/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersMicroserviceModule } from './users-microservice.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersMicroserviceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice(UsersMicroserviceModule, {
+    transport: Transport.TCP,
+    port: 3001,
+  });
+  await app.listen();
 }
 bootstrap();
